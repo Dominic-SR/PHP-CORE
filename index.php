a45cd717dc5841da8cb2b0b37bbfa38f03e338df
Dominic <Dominicseduraja3@gmail.com>
2021-12-30 21:06:32 +0530

    Update index.php

--- index.php
+++ index.php
@@ -3,11 +3,13 @@ session_start();
 
 include("connection/dbconnection.php");
 
+
 if(!isset($_SESSION['email']))
 {
-	header("location:Authentication/Login.php");
+header("location:Authentication/Login.php");
 }
 else{
     header("location:Pages/index.php");
 }
+
 ?>
